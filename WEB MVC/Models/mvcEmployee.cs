69545d45b121c4b3647bb48199a75fd213b88504
gauravvaghela <vaghelagaurav007@gmail.com>
2018-10-27 00:21:14 +0530

    Revert "Revert "ASP.NET MVC API""
    This reverts commit 8ae01d97775a7f51fbc4e276824676b4914f3836.

---
+++ WEB MVC/Models/mvcEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB_MVC.Models
+{
+    public class mvcEmployee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
